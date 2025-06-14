78302ca30702f06153ecab61867999fe77268c46
LeonPS <leon@leonps.com>
2025-06-14 16:49:41 +0100

    Setup with CocoaPods

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '16.0'
+
+target 'verovio-ios-example' do
+  use_frameworks!
+  pod 'Verovio', :git => 'https://github.com/rism-digital/verovio.git', :branch => 'develop'
+end
--- verovio-ios-example/VerovioIOSExampleApp.swift
+++ verovio-ios-example/VerovioIOSExampleApp.swift
@@ -2,6 +2,15 @@ import SwiftUI
 
 @main
 struct VerovioIOSExampleApp: App {
+
+    init() {
+        let meiURL = Bundle.main.url(
+            forResource: "010-1b-Sm-009", withExtension: "mei")!
+        let svg = VerovioWrapper().renderFirstPage(
+            url: meiURL, size: CGSize(width: 50, height: 50))
+        print(svg)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
